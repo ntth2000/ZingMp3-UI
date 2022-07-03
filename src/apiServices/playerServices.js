6aceb8ca090d73e11a1f8bea2c921edb5a057b85
ntth2000 <thaihuyennguyen2000@gmail.com>
2022-07-03 22:24:37 +0700

    fix url

---
+++ src/apiServices/playerServices.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import * as request from "~/utils/request";
+
+export const fetchSong = async (songId) => {
+  try {
+    const res = await request.get("song/" + songId);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchStreaming = async (songId) => {
+  try {
+    const res = await axios.get(REACT_APP_STREAMING_URL + songId);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
---
+++ src/utils/request.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export const request = axios.create({
+  baseURL: REACT_APP_FETCH_API,
+});
+export const get = async (url, options = {}) => {
+  const res = await request.get(url, options);
+  return res;
+};
+export const post = (url, body = {}) => {
+  return request
+    .post(url, body)
+    .then((res) => {
+      if (res.ok) {
+        console.log(res);
+        return res;
+      }
+    })
+    .catch((error) => {
+      console.log(error.response);
+      return error.response;
+    });
+};
